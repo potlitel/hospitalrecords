cc2041e13392640cd9e9f4f16d127535ef4ca2fe
potlitel <potlitel@gmail.com>
2022-06-06 20:39:42 -0400

    Se crean los componentes Footer.js, Topnav.js y Landing.js.

---
+++ pages/Components/Footer.js
@@ -0,0 +1,12 @@
+import {React, useEffect} from 'react';
+
+
+function Footer() {
+  return <footer className=' bg-blue-600 py-8 flex items-center justify-center mt-10'>
+    <div>
+        <h3 className=' text-white font-medium text-xl'>Made with StrapiCMS and NextJs</h3>
+    </div>
+  </footer>;
+}
+
+export default Footer;
